e4563b1fdfeb0e6c0dbad53f2bd36df3e8e2fc2d
rfyepes <98243446+rfyepes@users.noreply.github.com>
2022-04-19 10:19:32 -0500

    Add files via upload

---
+++ heroku-demo.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var port = process.env.PORT || 3000;
+console.log("This goes to the console window");
+http.createServer(function (req, res) {
+  console.log("server created");
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.write('<h2>Hello World<h2>');
+  res.write("Success! This app is deployed online");
+
+  res.end();
+}).listen(port);
